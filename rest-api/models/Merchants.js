8d51a946baa21ffb8e04a9813c2fdad41d2f5f73
Halil İbrahim Direktör <hidirektor@gmail.com>
2024-05-21 00:19:12 +0300

    Merchant

--- rest-api/models/Merchants.js
+++ rest-api/models/Merchants.js
@@ -4,6 +4,7 @@ const sequelize = require('../config/database');
 const Merchants = sequelize.define('Merchants', {
     id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
     merchantName: { type: DataTypes.STRING, allowNull: false },
+    merchantAddress: { type: DataTypes.STRING, allowNull: false },
     ownerUserName: { type: DataTypes.STRING, unique: true, allowNull: false },
     contactNumber: { type: DataTypes.STRING, allowNull: false }
 }, {
