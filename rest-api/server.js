a18c9f32754a51bdc7ecde20dbc4810766473253
Halil İbrahim Direktör <hidirektor@gmail.com>
2024-05-31 21:17:32 +0300

    Update Request Limit

--- rest-api/server.js
+++ rest-api/server.js
@@ -16,8 +16,8 @@ const orderRoutes = require('./routes/order');
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use(express.json());
 app.use('/api/v1/auth', authRoutes);
