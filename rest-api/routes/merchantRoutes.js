fb7bc2ad22f08f9edd898cf0fb2db59a91690b64
Halil İbrahim Direktör <hidirektor@gmail.com>
2024-06-19 23:17:07 +0300

    Merchant Authentication

--- rest-api/routes/merchantRoutes.js
+++ rest-api/routes/merchantRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const updateMerchantAPI = require('../controllers/merchant/updateMerchantAPI');
 const getMerchantAPI = require('../controllers/merchant/getMerchantAPI');
+const authMiddleware = require("../middlewares/authMiddleware");
 
-router.post('/updateMerchantAPI', updateMerchantAPI);
-router.post('/getMerchantAPI', getMerchantAPI);
+router.post('/updateMerchantAPI', authMiddleware, updateMerchantAPI);
+router.post('/getMerchantAPI', authMiddleware, getMerchantAPI);
 
 module.exports = router;
