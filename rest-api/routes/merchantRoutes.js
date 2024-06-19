7c9e3ad3899264c8592e82ea36c0b0e9ac47995d
Halil İbrahim Direktör <hidirektor@gmail.com>
2024-06-19 23:15:10 +0300

    Route Update

--- rest-api/routes/merchantRoutes.js
+++ rest-api/routes/merchantRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const updateMerchantAPI = require('../controllers/merchant/updateMerchantAPI');
 const getMerchantAPI = require('../controllers/merchant/getMerchantAPI');
 
-router.post('/updateMarketplaceAPI', updateMerchantAPI);
-router.post('/getMarketplaceAPI', getMerchantAPI);
+router.post('/updateMerchantAPI', updateMerchantAPI);
+router.post('/getMerchantAPI', getMerchantAPI);
 
 module.exports = router;
